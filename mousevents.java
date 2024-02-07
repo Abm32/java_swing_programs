13d08dcb2d92b8b3bfe0972001c23049d6affb26
Abhimanyu R B <abhimanyurbsa@gmail.com>
2024-02-07 21:06:14 +0530

    mouse events program using swing

---
+++ mousevents.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+class mousevents extends Frame implements MouseListener{
+	Label l;
+	mousevents(){
+		addMouseListener(this);
+		l = new Label();
+		l.setBounds(100,100,100,30);
+		add(l);
+		setSize(300,300);
+		setLayout(null);
+		setVisible(true);
+	}
+	public void mouseClicked(MouseEvent e){
+		l.setText("Mouse Clicked");
+	}
+	public void mouseEntered(MouseEvent e){
+		l.setText("Mouse Entered");
+	}
+	public void mouseExited(MouseEvent e){
+		l.setText("Mouse Exited");
+	}
+	public void mousePressed(MouseEvent e){
+		l.setText("Mouse Pressed");
+	}
+	public void mouseReleased(MouseEvent e){
+	}
+	public static void main(String args[]){
+		mousevents obj = new mousevents();
+	}
+}
